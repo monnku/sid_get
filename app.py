f5a02d97f84cbf601d33dcc066440167c1ace858
monnku <129637317+monnku@users.noreply.github.com>
2024-05-19 14:57:01 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ from testapp import app
 from flask import render_template, request, Flask
 app = Flask(__name__)
 
-def load_config(app, config_file='config.json'):
-    with open(config_file) as f:
-        config = json.load(f)
-        app.config.update(config)
-
-load_config(app)
-
 @app.route('/', methods=['GET'])
 def form():
     return render_template('templates/index.html')
